e22cd1981e3312d068a403f9d2de80bb9fb82236
Hamza Syed <sahamzashah19@gmail.com>
2023-01-19 21:05:09 +0500

    Updated

--- notepad.py
+++ notepad.py
@@ -1,9 +1,6 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
-from PyQt6.QtCore import *
 import sys
-from PyQt6.QtPrintSupport import QPrinter
-
 
 width = 1150
 height = 550
@@ -16,10 +13,6 @@ class MyMainWindow(QMainWindow):
         self.setMouseTracking(True)
         self.setFixedSize(width, height)
         my_menu = QMenuBar(self)
-        # self.shortcut_save = QShortcut(QKeySequence('Ctrl+S'), self)
-        # self.shortcut_save.activated.connect(self.save)
-        # self.shortcut_open = QShortcut(QKeySequence('Ctrl+O'), self)
-        # self.shortcut_open.activated.connect(self.open)
         my_menu.setStyleSheet(
             """
             QMenuBar
@@ -56,12 +49,9 @@ class MyMainWindow(QMainWindow):
             }
              """
         )
-        # my_menu = self.menuBar()
         my_menu.setObjectName("my_menu")
-        # my_menu.setStyleSheet("background-color:#EAF6F6")
         file_menu = my_menu.addMenu("File")
         file_menu.setObjectName("file")
-        # file_menu.setStyleSheet("background-color:#FFFFFF;padding-top:10;")
         new_file = QAction("New", self)
         new_file.setShortcut(QKeySequence("Ctrl+N"))
         new_window_file = QAction("New Window", self)
@@ -86,7 +76,7 @@ class MyMainWindow(QMainWindow):
         save_file.triggered.connect(self.save)
         save_as_file.triggered.connect(self.save_as)
         edit_menu = my_menu.addMenu("Edit")
-        undo_edit = QAction("Undo",self)
+        undo_edit = QAction("Undo", self)
         edit_menu.addAction(undo_edit)
         undo_edit.setShortcut(QKeySequence('Ctrl+Z'))
         undo_edit.triggered.connect(self.undof)
@@ -104,7 +94,7 @@ class MyMainWindow(QMainWindow):
         cut_edit.triggered.connect(self.cutf)
         view_menu = my_menu.addMenu("View")
         fontfam = view_menu.addMenu("Font")
-        timesnew = QAction("Times New Roman",self)
+        timesnew = QAction("Times New Roman", self)
         timesnew.triggered.connect(self.times)
         fontfam.addAction(timesnew)
         arial = QAction("Arial", self)
@@ -119,10 +109,8 @@ class MyMainWindow(QMainWindow):
         zoom.addAction(zoomout)
         status_bar = QAction("Status Bar", self)
         view_menu.addAction(status_bar)
-
-
-        # new.triggered.connect(self.test)
         self.b = QPlainTextEdit(self)
+        self.b.setStyleSheet("font-size:14px")
         self.b.textChanged.connect(self.textchanged)
         self.b.move(0, 40)
         self.b.resize(width, height + 50)
@@ -134,37 +122,37 @@ class MyMainWindow(QMainWindow):
         self.b.setStyleSheet("font-size:35px")
 
     def zoomout(self):
-        self.b.setStyleSheet("font-size:12px")
+        self.b.setStyleSheet("font-size:14px")
+
     def times(self):
         self.b.setStyleSheet("font-family:Times New Roman")
 
     def arial(self):
         self.b.setStyleSheet("font-family:Arial")
+
     def cutf(self):
         QApplication.clipboard().setText(self.b.toPlainText())
         self.b.setPlainText("")
+
     def copyf(self):
         QApplication.clipboard().setText(self.b.toPlainText())
+
     def printf(self):
-        self.b.setPlainText(self.b.toPlainText()+QApplication.clipboard().text())
+        self.b.setPlainText(self.b.toPlainText() + QApplication.clipboard().text())
+
     def undof(self):
         a = self.b.toPlainText()
-        b = len(a)-1
+        b = len(a) - 1
         c = a[:b]
         self.b.setPlainText(c)
+
     def textchanged(self):
         a = self.b.toPlainText()
         b = len(a)
-        self.statusBar.showMessage("    No. of Words: "+str(b))
+        self.statusBar.showMessage("    No. of Words: " + str(b))
 
-    def counter(self):
-        no = len(self.b.toPlainText())
-        if no>0:
-            return no
-        else:
-            return 69
     def new_file_func(self):
-        new_window.show()
+        window.show()
 
     def reset(self):
         self.b.setPlainText("")
@@ -183,11 +171,6 @@ class MyMainWindow(QMainWindow):
                 file.write(data)
         except:
             window.show()
-        # text = QInputDialog.getText(self, 'Save File', 'Enter File name:')
-        # output = str(text[0]) + '.txt'
-        # data = self.b.toPlainText()
-        # with open(output, 'w') as file_data:
-        #     file_data.write(data)
 
     def save_as(self):
         text = QInputDialog.getText(self, 'Save File', 'Enter File name:')
@@ -207,24 +190,6 @@ class MyMainWindow(QMainWindow):
                 self.b.setPlainText(output)
         except:
             window.show()
-        # text = QInputDialog.getText(self, 'Open File', 'Enter File name:')
-
-        # file_path = "D:" + chr(92) + "Advance Programming Techniques" + chr(92) + "Programs" + chr(92) + str(
-        #     text[0]) + '.txt'
-        # if os.path.exists(file_path):
-        #     with open(file_path, 'r') as file_data:
-        #         output = file_data.read()
-        #         self.b.setPlainText(output)
-        # else:
-        #     print("Path Doesnt Exist")
-        # msg = QMessageBox()
-        # msg.setIcon(QMessageBox.Information)
-        #
-        # msg.setText("This is a message box")
-        # msg.setInformativeText("This is additional information")
-        # msg.setWindowTitle("MessageBox demo")
-        # msg.setDetailedText("The details are as follows:")
-        # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
 
 app = QApplication([])
